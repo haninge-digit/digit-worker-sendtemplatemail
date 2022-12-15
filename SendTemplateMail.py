530839b129321d7b5621e4fd37c167c4f6a843a4
Håkan Persson <hakan.persson@haninge.se>
2022-12-15 12:27:13 +0100

    Manually added som stuff from main branch

--- SendTemplateMail.py
+++ SendTemplateMail.py
@@ -45,7 +45,7 @@ class SendTemplateMail(object):
     """
     Init function. Nothing here so far.
     """
-    def __init__(self, async_loop):
+    def __init__(self):
         pass
 
 
@@ -53,7 +53,7 @@ class SendTemplateMail(object):
         stand_alone = '_STANDALONE' in vars
 
         if 'mailRecipient' not in vars:
-            return self._handle_error(stand_alone, "Variable 'mailRecipient' is missing")
+            return self._handle_worker_error(stand_alone, "Variable 'mailRecipient' is missing")
 
         if 'JSON_DUMP' in vars:         # Just do a JSON-dump of the whole stuff
             body = json.dumps(json.loads(vars['_JSON_BODY']),indent=2)      # Stupid way to format JSON?   
@@ -68,13 +68,13 @@ class SendTemplateMail(object):
 
             try:
                 res = await httpx.AsyncClient().get(template_url)   # Read the teamplate from some external public storage (Github)
-            except httpx.ReadTimeout as e:
-                return self._handle_error(stand_alone, f"load_template() raised httpx.ReadTimeout")
+            except httpx.ReadTimeout as e:      # ToDo: More error handling
+                return self._handle_worker_error(stand_alone, f"load_template() raised httpx.ReadTimeout")
             if res.status_code != 200:
                 if res.status_code == 404:
-                    return self._handle_error(stand_alone, f"Template {template_url} not found!")
+                    return self._handle_worker_error(stand_alone, f"Template {template_url} not found!")
                 else:
-                    return self._handle_error(stand_alone, f"get template returned {res.status_code}")
+                    return self._handle_worker_error(stand_alone, f"get template returned {res.status_code}")
 
             email_template = jinja2.Environment().from_string(res.text)     # Load template into Jinja
 
@@ -87,11 +87,7 @@ class SendTemplateMail(object):
                 rendered_content = email_template.render(render_variables)      # Render the template with Jinja2
             except Exception as e:         # Render will fail if data and template doesn't match!
                 loggtext = f"Template render failed with error: {e}. Call contained a JSON body = {'_JSON_BODY' in vars}"
-                logging.error(loggtext)
-                if '_STANDALONE' in vars:   # This is a single worker
-                    return {'_DIGIT_ERROR': loggtext}     # We can return an error
-                raise WorkerError(loggtext, retries=0)       # Fatal! Halt the process
-
+                self._handle_worker_error(stand_alone, loggtext)
         if 'ATTACH_PDF' in vars:        # Ww can also check for things like "ATTACH_PDF=email" meaning the content of the email as PDF
             pdf = weasyprint.HTML(string=rendered_content).write_pdf()      # Which is the only thing we implemen right now
         else:
@@ -113,27 +109,27 @@ class SendTemplateMail(object):
         if pdf:
             attachment = MIMEApplication(pdf,'application/pdf')
             attachment.add_header('Content-Disposition', 'attachment', filename=f"{mail_subject}.pdf")
-            message.attach(attachment)            
+            message.attach(attachment)  
 
-        credential = ClientSecretCredential(AD_TENANT_ID, AD_CLIENT_ID, AD_CLIENT_SECRET)
-        client = GraphClient(credential=credential)         # Get a authenticated Graph client. Might be better to have one for the whole worker?
-        userPrincipalName = "noreply@haninge.se"            # This is the user account that our mail are sent from
+        try:
+            credential = ClientSecretCredential(AD_TENANT_ID, AD_CLIENT_ID, AD_CLIENT_SECRET)
+            client = GraphClient(credential=credential)         # Get a authenticated Graph client. Might be better to have one for the whole worker?
+            userPrincipalName = "noreply@haninge.se"            # This is the user account that our mail are sent from
 
-        result = client.post(f"/users/{userPrincipalName}/sendMail", 
-                            data=base64.b64encode(message.as_string().encode('utf-8')),
-                            headers={'Content-Type': 'text/plain'})
-        if 'error' in result:
-            loggtext = f"sendMail failed! {result['error']['code']: {result['error']['message']}}"
-            logging.error(loggtext)
-            if '_STANDALONE' in vars:
-                return {'_DIGIT_ERROR': loggtext}       # This can be returned to the caller
-            else:
-                raise WorkerError(loggtext, retries=0)          # In a worklfow so cancel further processeing
+            result = client.post(f"/users/{userPrincipalName}/sendMail", 
+                                data=base64.b64encode(message.as_string().encode('utf-8')),
+                                headers={'Content-Type': 'text/plain'})
+            if 'error' in result:
+                loggtext = f"sendMail failed! {result['error']['code']: {result['error']['message']}}"
+                self._handle_worker_error(stand_alone, loggtext)
+        except Exception as e:         # Some error
+            loggtext = f"Send mail failed with error: {e}"
+            self._handle_worker_error(stand_alone, loggtext)
 
         return {}
 
 
-    def _handle_error(self,stand_alone,loggtext):
+    def _handle_worker_error(self,stand_alone,loggtext):
         logging.error(loggtext)
         if stand_alone:
             return {'_DIGIT_ERROR': loggtext}       # This can be returned to the caller
